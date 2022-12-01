4e6b01c394cffa4338b0a90642869416003be8bf
DanilaBratinov <bratinov0704@inbox.ru>
2022-12-01 12:15:58 +0300

    Initial Commit

---
+++ RegisterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegisterApp
+//
+//  Created by Данила Братинов on 01.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
